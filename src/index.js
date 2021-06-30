0e739019a260366ef6862b9fff735e01f724b86c
peeech <pj@imagine-nyc.com>
2021-06-30 15:03:29 +0200

    communicate with db WIP

--- src/index.js
+++ src/index.js
@@ -3,14 +3,19 @@ const { closeDb } = require('./database')
 
 async function run() {
   // Get all holoports registered for testing
+  // TODO - create collection
 
   // Get their (and only their) details from latest_zt_snap
+  // TODO - how to filter collection.find() using an array of items?
 
   // Then filter out stale or incorrect entries
+  // TODO - why is data duplicated and what is invalid data?
 
   // Then ssh-ping and record outcome
+  // TODO - in a truly async style
 
   // Then upload entries into appropriate collection
+  // TODO - create collection [name, IP, timestamp, ssh-outcome, HoloNet]
 
   // *** Tinkering ***
   const collection = await getCollection('latest_zt_snap')
