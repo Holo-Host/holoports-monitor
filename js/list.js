3ac3a8d7ce42c1a375b2057b60e87e4563256037
peeech <pj@imagine-nyc.com>
2023-01-26 14:45:44 +0100

    show errors nicely

--- js/list.js
+++ js/list.js
@@ -4,8 +4,18 @@ async function getData() {
   return availableHoloportsDetails;
 }
 
+function show_modal(arr) {
+  document.querySelector('#modal-inner').innerHTML = arr;
+  document.querySelector('#modal').style.display = 'block';
+  document.querySelector('#modal').style.top = window.scrollY + 20 + 'px';
+}
+
+function hide_modal() {
+  document.querySelector('#modal').style.display = 'none';
+}
+
 function timeSince(date) {
-  const seconds = Math.floor(new Date() / 1000) - date;
+  const seconds = Math.floor(new Date() / 1000) - date / 1000;
   let interval = seconds / 31536000;
 
   if (interval > 1) {
@@ -43,8 +53,9 @@ function getSourceChains(hostingInfo) {
   return result;
 }
 
-function printRow(hp) {
-  hp.timestamp = timeSince(hp.timestamp) + ` ago`;
+function printRow(hp, i) {
+  hp.debug = hp.lastZerotierOnline;
+  hp.lastZerotierOnline = timeSince(hp.lastZerotierOnline) + ` ago`;
   hp.totalSourceChains = getSourceChains(hp.hostingInfo);
 
   let successCheck;
@@ -58,17 +69,25 @@ function printRow(hp) {
     hp[key] = (hp[key]===null)?"-":hp[key];
   }
 
+  let errorRow
+  if (hp.errors.length === 0) {
+    errorRow = '<td class="success">no</td>'
+  } else {
+    errorRow = '<td class="warning"><span onClick="alert(data['+i+'].errors)">YES</span></td>'
+  }
+
   return `
     <tr>
-      <td class="too-long" title="${hp.holoportId}">${hp.holoportId}</td>
-      <td>${hp.ztIp}</td>
-      <td>${hp.timestamp}</td>
+      <td title="${hp.holoportId}">${hp.holoportId}</td>
+      <td>${hp.zerotierIp}</td>
+      <td>${hp.lastZerotierOnline}</td>
       <td>${hp.sshStatus}</td>
       <td>${hp.holoNetwork}</td>
       <td>${hp.channel}</td>
       <td title="${hp.holoportModel}">${hp.holoportModel}</td>
-      <td class="too-long"><div>${hp.channelVersion}</div></td>
-      <td class="too-long"><div>${hp.hposVersion}</div></td>
+      <td class="too-long"><div onClick="alert('${hp.channelVersion}')">${hp.channelVersion.substring(0,7)}</div></td>
+      <td class="too-long"><div onClick="alert('${hp.hposVersion}')">${hp.hposVersion.substring(0,7)}</div></td>
+      ${errorRow}
     </tr>
   `;
 }
@@ -79,19 +98,20 @@ function buildTable(hps) {
       <tr>
           <th>HoloPort Id</th>
           <th>Zerotier IP</th>
-          <th>Timestamp</th>
+          <th>Last seen on ZT</th>
           <th>SSH enabled</th>
           <th>Holo Network</th>
           <th>Channel</th>
           <th>Model</th>
           <th>Channel Version</th>
           <th>HPOS Version</th>
+          <th>Errors</th>
       </tr>
     </thead>
   <tbody>`;
 
-  for (const hp of hps) {
-    innerHtml += printRow(hp);
+  for (let i = 0; i < data.length; i++) {
+    innerHtml += printRow(data[i], i);
   }
 
   innerHtml += `</tbody>`;
@@ -101,16 +121,19 @@ function buildTable(hps) {
 
 getData()
   .then(data => {
+    // makes data available to js outside of this code block
+    window.data = data;
     buildTable(data);
 
     const filtersConfig = {
-        base_path: '/tablefilter/',
+        base_path: '/holoports/tablefilter/',
         col_3: 'select',
         col_4: 'select',
         col_5: 'select',
         col_6: 'select',
         col_7: 'select',
         col_8: 'select',
+        col_9: 'select',
         alternate_rows: true,
         rows_counter: true,
         mark_active_columns: true,
@@ -122,7 +145,7 @@ getData()
         col_widths: [
             '450px', '130px', '100px', '100px',
             '100px', '100px', '100px',
-            '120px', '100px', '120px'
+            '120px', '100px', '70px'
         ],
         extensions:[{ name: 'sort' }]
     };
